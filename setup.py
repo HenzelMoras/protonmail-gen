e5579fc4798f6f0a2f591303e745b383b1787fd5
HenzelMoras <henzelmoras@gmail.com>
2021-03-13 02:52:54 -0500

    setup is created :package:

---
+++ setup.py
@@ -0,0 +1,28 @@
+import subprocess
+import sys
+from __DownloadDrivers__.download import *
+
+def install(name):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', name])
+
+def main():
+
+    my_packages = ['requests', 'clint', 'faker', 'selenium', 'colorama']
+
+    for package in my_packages:
+        install(package)
+        print('\n')
+    
+    print('checking whether Firefox is installed')
+    firefox_ver = get_firefox_version()
+    if firefox_ver != None:
+        print(' firefox is installed')
+        print(' proceeding to with geckodriver setup')
+        setup_firefox(firefox_ver)
+    else:
+        print('Error - Firefox is not installed ... install firefox to proceed!')
+        exit()
+    print("Setup Completed")    
+
+if __name__== '__main__':
+    main()
