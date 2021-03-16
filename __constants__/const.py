fb9dd0b29ea86aa1f66fbc298f41486a51a79509
HenzelMoras <henzelmoras@gmail.com>
2021-03-16 09:33:44 -0400

    add email var

--- __constants__/const.py
+++ __constants__/const.py
@@ -1,5 +1,7 @@
 import sys
 import subprocess
+from random import choice
+import time
 try:
     from password_generator import PasswordGenerator
 except ImportError:
@@ -8,6 +10,8 @@ try:
     from faker import Faker
 except ImportError:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'faker'])
+
+
 
 fake = Faker('en_US')
 username = fake.name().replace(" ", fake.color_name())
@@ -19,5 +23,19 @@ confirm_pass = password
 
 url = 'https://mail.protonmail.com/create/new?language=en'
 
+domains = [
+	'getnada.com', 
+	'amail.club', 
+	'banit.club',
+	'cars2.club',
+	'cmail.club',
+	'banit.me',
+	'duck2.club',
+	'nada.email',
+	'nada.ltd',
+	'wmail.club'
+]
 
+email = username + '@' + choice(domains) 
+service = 'https://getnada.com/api/v1/inboxes/'
 
