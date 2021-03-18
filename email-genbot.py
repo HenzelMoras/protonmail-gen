cd24444d4530cfe605a081e0512d41563dd28b87
HenzelMoras <henzelmoras@gmail.com>
2021-03-18 05:01:19 -0400

    made changes using __functions__

--- email-genbot.py
+++ email-genbot.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 import time
-import os.path
-from os import path
+
 from datetime import datetime
-from __constants__.const import url, username, password, confirm_pass, email, service
+from __constants__.const import url, username, password, confirm_pass, email, api, options,driver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,17 +12,6 @@ from selenium.webdriver.support.ui import Select
 import requests
 from bs4 import BeautifulSoup as bs
 from __functions__.functions import inject_input, scroll_click_element, switch_frame, click_element
-
-try:
-    if path.exists('./webdriver/geckodriver'):
-        driver = webdriver.Firefox(executable_path=r'./webdriver/geckodriver')          
-    else:
-        raise Exception('Driver not found .. Run Setup.py first')
-except Exception as e:
-    time.sleep(0.4)
-    print('Error - '+ str(e))
-    exit()
-
 
 driver.get(url)
 time.sleep(1)
@@ -42,8 +30,6 @@ inject_input(driver, password, id='password')
 # confirm_pass
 inject_input(driver, confirm_pass, id='passwordc')
 
-
-
 # create account
 switch_frame(driver, class_name="bottom")
 
@@ -53,68 +39,54 @@ scroll_click_element(driver, '/html/body/div/div/footer/button')
 
 # confirm creation 
 driver.switch_to.default_content()
+click_element(driver, '//*[@id="confirmModalBtn"]')
 
-WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-        (By.XPATH, '//*[@id="confirmModalBtn"]')))
+# check bot abuse 
+try:
+        click_element(driver, '//*[@id="verification-panel"]/div[3]/label/div')
+        click_element(driver, '//*[@id="verification-panel"]/div[2]/label/div')
+except:
+        print("Looks like YOU have abused the bot!! Try Again later")
+        input("Press enter/return key to exit.")
+        driver.close()
 
-driver.find_element_by_xpath('//*[@id="confirmModalBtn"]').click()   
-
-
-# Email verification selection
-click_element(driver, '//*[@id="id-signup-radio-email"]')
-
-"""
 # fill email 
-email_verification = '//*[@id="emailVerification"]'
+scroll_click_element(driver, '//*[@id="emailVerification"]')
+inject_input(driver, email ,xpath='//*[@id="emailVerification"]')
+scroll_click_element(driver, '//*[@id="verification-panel"]/form[1]/div[1]/div[2]/button')
 
-WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-        (By.XPATH, email_verification)))
-
-driver.find_element_by_xpath(email_verification).send_keys(email)
-
-
-click_send_box = '/html/body/div[2]/div/div/div/form/div/div/form[1]/div[1]/div[2]/button'
-
-
-# send the email for verification
-driver.find_element_by_xpath(click_send_box).click()
-
-# check tem-mail box and retrive code
+# get verification code from disposable mail
 while True:
-    try:
-        uid = requests.get(f"https://getnada.com/api/v1/inboxes/{email}").json()
-        uid = uid["msgs"][0]["uid"]
-        print(uid)
-        break
-    except:
-        time.sleep(1)
-        pass
-
-html = requests.get(f"https://getnada.com/api/v1/messages/html/{uid}").content
+        try:
+            uid = requests.get(f"{api}inboxes/{email}").json()
+            uid = uid["msgs"][0]["uid"]
+            print('uid: ',uid)
+            break
+        except:
+            time.sleep(1)
+            pass
+html = requests.get(f"{api}messages/html/{uid}").content
 soup = bs(html,"html5lib")
 code = soup.find("code").text
-print(code)
+    
+scroll_click_element(driver, '//*[@id="codeValue"]')
+inject_input(driver, code, css_selector='input[type = "text"]')
 
-# submit verification
-verification_code = '//*[@id="codeValue"]'
+scroll_click_element(driver, '//*[@id="verification-panel"]/p[3]/button') # Complete setup btn
+time.sleep(1)
 
-WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-        (By.XPATH, verification_code)))
+scroll_click_element(driver, '//*[@id="confirmModalBtn"]')
 
-driver.find_element_by_xpath(verification_code).send_keys('123')
-"""
-# complete setup
+for _ in range(0, 3): 
+        scroll_click_element(driver, '//*[@id="pm_wizard"]/div/div[5]/button[1]')
+        time.sleep(.5)
+        scroll_click_element(driver, '//*[@id="pm_wizard"]/div/div[5]/button[2]')
 
-complete_Setup = '/html/body/div[2]/div/div/div/form/div/div/p[3]/button'
+print("\nAccount Details.\n")
 
-code_input = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type = "text"]')))
-for i in code_input:
-        code_input.send_keys(i)
-        time.sleep(.1)
-
-
-print(' \nsaving account details\n')
-with open('account.txt', 'a') as f:
+username = "Username: " + username
+password = "Password: " + password
+with open("Accounts.txt", 'a') as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M")+"\n")
         f.write(username+"\n")
         f.write(password+"\n")
